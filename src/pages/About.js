f163185db4719b659e9e139b8db17f3b6cc51b9b
bekzodnj <bekzodnx@gmail.com>
2020-08-17 21:44:18 +0100

    Font edited in About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,11 +3,11 @@ import './../App.css';
 
 function About() {
   return (
-    <div className='pv5 bb b--white-60'>
-      <h1 className='f2' style={{ fontWeight: 400 }}>
+    <div className='pv5 pv6-ns bb b--white-60'>
+      <h1 className='f3 f2-ns' style={{ fontWeight: 400 }}>
         About Me
       </h1>
-      <p className='f4 lh-copy measure mw7'>
+      <p className='f6 f4-ns lh-copy measure  mw7'>
         My name is Bekzodjon and I'm a Frontend Developer Intern at Ericsson. I
         mostly work with JavaScript, React, CSS, Express.js.
       </p>
