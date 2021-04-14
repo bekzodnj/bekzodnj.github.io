3d537d521bff81f168a8f5521ae85d9fa4f37b3f
Bekzodjon Norkuziev <bekzodnx@gmail.com>
2021-04-14 15:33:36 +0200

    Updates

---
+++ precache-manifest.64ed4406c7b1efda2c838eb95f98c3f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3949119364d10e9bec9f065d8c6bb4a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87cbcc3d46f45c3f15bf",
+    "url": "/static/css/2.a37d67d4.chunk.css"
+  },
+  {
+    "revision": "c037df54c175e21079df",
+    "url": "/static/css/main.ccff9c28.chunk.css"
+  },
+  {
+    "revision": "87cbcc3d46f45c3f15bf",
+    "url": "/static/js/2.4d25f1bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4d25f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c037df54c175e21079df",
+    "url": "/static/js/main.8c540407.chunk.js"
+  },
+  {
+    "revision": "da184adb2e2b397f1ce9",
+    "url": "/static/js/runtime-main.c2a424fd.js"
+  }
+]);
